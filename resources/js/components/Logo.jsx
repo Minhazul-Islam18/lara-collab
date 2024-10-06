94577c299144fc7a295901afa75c21bea368fd58
Minhazul Islam <minhaz019166@gmail.com>
2024-10-06 07:27:44 +0600

    lara collab removed

--- resources/js/components/Logo.jsx
+++ resources/js/components/Logo.jsx
@@ -1,21 +1,27 @@
-import { Center, Group, Text, rem, useComputedColorScheme } from "@mantine/core";
-import { IconChartArcs } from "@tabler/icons-react";
+import { Center, Group, Text, rem, useComputedColorScheme } from '@mantine/core';
+import { IconChartArcs, IconTableSpark } from '@tabler/icons-react';
 
 export default function Logo(props) {
   const computedColorScheme = useComputedColorScheme();
 
   return (
-    <Group wrap="nowrap" {...props}>
+    <Group
+      wrap='nowrap'
+      {...props}
+    >
       <Center
-        bg={computedColorScheme === "dark" ? "blue.8" : "blue.9"}
+        bg={computedColorScheme === 'dark' ? 'blue.8' : 'blue.9'}
         p={5}
-        style={{ borderRadius: "100%" }}
+        style={{ borderRadius: '100%' }}
       >
-        <IconChartArcs style={{ stroke: "#fff", width: rem(25), height: rem(25), flexShrink: 0 }} />
+        <IconTableSpark
+          style={{ stroke: '#fff', width: rem(25), height: rem(25), flexShrink: 0 }}
+        />
       </Center>
-      <Text fz={20} fw={600}>
-        LaraCollab
-      </Text>
+      <Text
+        fz={20}
+        fw={600}
+      ></Text>
     </Group>
   );
 }
